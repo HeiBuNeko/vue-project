d12a50d9e2f736695f417e2b55f262e4aa76e605
猫宫黑布 <88360720+HeiBuNeko@users.noreply.github.com>
2023-12-29 12:02:36 +0800

    element vant vite5.0

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import persist from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 
+// element暗黑模式
+import 'element-plus/theme-chalk/dark/css-vars.css'
+// vant桌面端适配
+import '@vant/touch-emulator'
+
 const app = createApp(App)
 
 app.use(createPinia().use(persist))
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,43 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from '@vant/auto-import-resolver'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+    AutoImport({
+      resolvers: [
+        ElementPlusResolver(),
+        // 自动导入图标组件
+        IconsResolver({
+          prefix: 'Icon'
+        })
+      ]
+    }),
+    // 自动注册相关组件
+    Components({
+      resolvers: [
+        ElementPlusResolver(),
+        VantResolver(),
+        // 自动注册图标组件
+        IconsResolver({
+          enabledCollections: ['ep']
+        })
+      ]
+    }),
+    Icons({
+      autoInstall: true
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
